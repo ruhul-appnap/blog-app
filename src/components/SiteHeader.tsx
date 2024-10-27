5787d926ced7fa9524b99601862b53236cac25c9
Ruhul <ruhul@appnap.io>
2024-10-27 19:20:09 +0600

    Add theming

--- src/components/SiteHeader.tsx
+++ src/components/SiteHeader.tsx
@@ -4,7 +4,7 @@ import { Nav } from "./Nav";
 
 export function SiteHeader() {
   return (
-    <div className="rounded-2xl bg-gray-800/95 py-2 pl-2.5 pr-6 shadow-surface-glass backdrop-blur [@supports(backdrop-filter:blur(0px))]:bg-white/[3%]">
+    <div className="rounded-2xl bg-primary text-foreground">
       <div className="flex items-center justify-between">
         <Link href="/" title="Navigate home" className={clsx("rounded-full")}>
           <div>Logo</div>
